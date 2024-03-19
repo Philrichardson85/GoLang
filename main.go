63bfe81809a283f49764ba380794a6cee06c6d50
Phillip Richardson <philrichardson85@gmail.com>
2024-03-19 18:47:00 -0400

    switched to new tutorial

--- main.go
+++ main.go
@@ -1,42 +1,17 @@
 package main
 
 import (
-	"encoding/json"
-	"encoding/xml"
-	"fmt"
-	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-type Customer struct {
-	Name string `json:"full_name" xml:"name"`
-	City string `json:"city" xml:"city"`
-	Zipcode string `json:"zip_code" xml:"zip_code"`
-}
-
 func main() {
-
-	http.HandleFunc("/greet", greet)
-	http.HandleFunc("/customers", getAllCustomers)
-
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))	
-}
-
-func greet(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "Hello World!!")
-}
-
-func getAllCustomers(w http.ResponseWriter, r *http.Request)  {
-	customers := []Customer {
-		{"Ash","Atlanta", "32303"},
-		{"Tom","Athens", "21303"}, 
-	}
-
-	if r.Header.Get("Content-Type") == "application/xml" {
-		w.Header().Add("Content-Type", "application/xml")
-		xml.NewEncoder(w).Encode(customers)
-	} else {
-		w.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(customers)
-	}
+  r := gin.Default()
+  r.GET("/ping", func(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+      "message": "pong",
+    })
+  })
+  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
