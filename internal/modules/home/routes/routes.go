3829e54cfa055df04be854306aeb29009ecb8442
Phillip Richardson <philrichardson85@gmail.com>
2024-03-20 21:47:51 -0400

    Added template files and controller

--- internal/modules/home/routes/routes.go
+++ internal/modules/home/routes/routes.go
@@ -1,17 +1,13 @@
 package routes
 
 import (
-	"demoBlog/pkg/html"
-	"net/http"
+	homeCtrl "demoBlog/internal/modules/home/controllers"
 
 	"github.com/gin-gonic/gin"
 )
 
 //set routes
 func Routes(router *gin.Engine)  {
-		router.GET("/", func(c *gin.Context) {
-			html.Render(c, http.StatusOK, "modules/home/html/home", gin.H{
-				"title": "Phil's Home website",
-			})
-		})
+	homeController := homeCtrl.New()
+		router.GET("/", homeController.Index)
 }
