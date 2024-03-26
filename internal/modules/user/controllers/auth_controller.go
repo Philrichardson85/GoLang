bfe50c4ac226f5d7fa4386bdd4af124e7d821762
Phillip Richardson <philrichardson85@gmail.com>
2024-03-25 22:09:20 -0400

    Added login logic

--- internal/modules/user/controllers/auth_controller.go
+++ internal/modules/user/controllers/auth_controller.go
@@ -74,3 +74,50 @@ func (controller *Controller) HandleRegister( c *gin.Context) {
 	log.Printf("the user created successfully with a name %s \n", user.Name)
 	c.Redirect(http.StatusFound, "/")
 }
+
+func (controller *Controller) Login(c *gin.Context) {
+	html.Render(c, http.StatusOK, "modules/user/html/login", gin.H{
+		"title": "Login",
+	})
+}
+
+func (controller *Controller) HandleLogin(c *gin.Context) {
+	var request auth.LoginRequest
+
+	if err := c.ShouldBind(&request); err != nil {
+		errors.Init()
+		errors.SetFromErrors(err)
+
+		sessions.Set(c, "errors", converters.MapToString(errors.Get()))
+		
+		old.Init()
+		old.Set(c)
+		sessions.Set(c, "old",converters.UrlValuesToString(old.Get()))
+
+
+		c.Redirect(http.StatusFound, "/login")
+		return
+	}
+
+	user, err := controller.userService.HandleUserLogin(request)
+
+	if err != nil {
+		errors.Init()
+		errors.Add("email", err.Error())
+
+		sessions.Set(c, "errors", converters.MapToString(errors.Get()))
+		
+		old.Init()
+		old.Set(c)
+		sessions.Set(c, "old", converters.UrlValuesToString(old.Get()))
+
+
+		c.Redirect(http.StatusFound, "/login")
+		return
+	}
+
+	sessions.Set(c, "auth", strconv.Itoa(int(user.ID)))
+
+	log.Printf("the user logged in successfully with a name %s \n", user.Name)
+	c.Redirect(http.StatusFound, "/")
+}
